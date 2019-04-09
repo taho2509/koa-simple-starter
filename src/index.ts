4eb08dcc1dcce24cd8f6142a44b4c168c86e8483
Ernesto Fuentes <ernesto.fuentes@peanuthub.cl>
2019-04-09 01:13:01 -0400

    feature(lint): added eslint typescript prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import app from './server'
+import logger from '@src/utils/logger'
 
-process.on('uncaughtException', e => {
-  console.log(e)
-  process.exit(1)
-})
-process.on('unhandledRejection', e => {
-  console.log(e)
-  process.exit(1)
-})
+process.on(
+  'uncaughtException',
+  (e: Error): void => {
+    logger.error('Uncaugth Exception :', e)
+    process.exit(1)
+  },
+)
+process.on(
+  'unhandledRejection',
+  (reason, p): void => {
+    logger.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason)
+    process.exit(1)
+  },
+)
 
 app.boot()
 app.on('application:booted', app.init)
