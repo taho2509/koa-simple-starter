523af27504e5aac7abcbafed5672c087a21b052b
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2022-01-29 10:51:56 -0300

    fix(logger): use silent instead of level none

--- src/interfaces/tools/logger/index.ts
+++ src/interfaces/tools/logger/index.ts
@@ -25,10 +25,10 @@ const logger: Logger = winston.createLogger({
 
 const appLogger = logger as WinstonLogger
 appLogger.mute = function (): void {
-  this.level = 'none'
+  this.silent = true
 }
 appLogger.unmute = function (): void {
-  this.level = config.get('LOG_LEVEL').toString()
+  this.silent = false
 }
 
 export default appLogger
